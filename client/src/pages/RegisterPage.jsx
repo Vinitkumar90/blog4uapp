79125485e69bc7647c58031a60ac510c09fb00fa
vinit <vinitkumar93341@gmail.com>
2025-04-26 15:38:38 +0530

    register-login page

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-function LoginPage() {
+function RegisterPage() {
   return (
-    <form className='login'>
-        <h1>Login</h1>
+    <form className='register'>
+        <h1>Register</h1>
         <input type="text" placeholder='username' />
         <input type="text" placeholder='password' />
-        <button>Login</button>
+        <button>Register</button>
     </form>
   )
 }
 
-export default LoginPage
+export default RegisterPage
